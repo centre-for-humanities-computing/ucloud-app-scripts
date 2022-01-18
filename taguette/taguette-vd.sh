2fbd1cf14768fa1180bf687ef231d04c576bd090
Kristoffer L. Nielbo <kln@cas.au.dk>
2022-01-18 14:03:44 +0100

    rm confirmation

--- taguette/taguette-vd.sh
+++ taguette/taguette-vd.sh
@@ -14,9 +14,3 @@ sudo chmod u+x ~/Desktop/taguette.desktop
 
 # run the app. We need to run in background (&) for ucloud desktop app to start
 taguette &
-
-# confirmation
-bold=$(tput bold)
-normal=$(tput sgr0)
-echo "----------------------- CHCAA says Taguette gogo -----------------------"
-echo "${bold}[INFO] You can now press Open Interface...${normal}"
