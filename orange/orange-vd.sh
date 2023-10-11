d5ff086856343937eb730f76302a8c53701e5bf1
Peter Vahlstrup <Imvpbv@cc.au.dk>
2023-10-11 20:00:51 +0200

    Update orange-vd.sh
    fix the broken script

--- orange/orange-vd.sh
+++ orange/orange-vd.sh
@@ -1,5 +1,17 @@
+sudo apt-get update -y
+sudo apt-get install libqt5gui5 -y
+pip install PyQt5 PyQtWebEngine
 pip install orange3
-#python -m Orange.canvas
+
+cat > ~/Desktop/orange3.sh << EOF
+python -m Orange.canvas
+EOF
+
+sudo chmod ugo+x ~/Desktop/orange3.sh
+
+# run it once to make it complain and terminate because of some missing settings.. it fixes itself though..., the user can after this run it from the desktop
+~/Desktop/orange3.sh
+
 # confirmation
 bold=$(tput bold)
 normal=$(tput sgr0)
